42b5b908de09bafb367e02ee15a60482831d9a61
naidu rampam <naidurampam@5555@gmail.com>
2024-07-31 12:41:47 +0000

    ListAndKeysCodeCls

---
+++ List-and-Keys-class/src/App.js
@@ -0,0 +1,89 @@
+import UserProfile from './components/UserProfile/index'
+import './App.css'
+
+const userDetailsList = [
+  {
+    uniqueNo: 1,
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/esther-howard-img.png',
+    name: 'Naidu',
+    role: 'Full Stack Developer',
+  },
+  {
+    uniqueNo: 2,
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/floyd-miles-img.png',
+    name: 'G Suji',
+    role: 'Auto Testing',
+  },
+  {
+    uniqueNo: 3,
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/jacob-jones-img.png',
+    name: 'Sai',
+    role: 'Web Developer',
+  },
+  {
+    uniqueNo: 4,
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/devon-lane-img.png',
+    name: 'Srinu',
+    role: 'Cloud Developer',
+  },
+  {
+    uniqueNo: 1,
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/esther-howard-img.png',
+    name: 'Naidu',
+    role: 'Full Stack Developer',
+  },
+  {
+    uniqueNo: 2,
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/floyd-miles-img.png',
+    name: 'G Suji',
+    role: 'Auto Testing',
+  },
+  {
+    uniqueNo: 3,
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/jacob-jones-img.png',
+    name: 'Sai',
+    role: 'Web Developer',
+  },
+  {
+    uniqueNo: 4,
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/devon-lane-img.png',
+    name: 'Srinu',
+    role: 'Cloud Developer',
+  },
+  {
+    uniqueNo: 1,
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/esther-howard-img.png',
+    name: 'Naidu',
+    role: 'Full Stack Developer',
+  },
+  {
+    uniqueNo: 2,
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/floyd-miles-img.png',
+    name: 'G Suji',
+    role: 'Auto Testing',
+  },
+  {
+    uniqueNo: 3,
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/jacob-jones-img.png',
+    name: 'Sai',
+    role: 'Web Developer',
+  },
+  {
+    uniqueNo: 4,
+    imageUrl: 'https://assets.ccbp.in/frontend/react-js/devon-lane-img.png',
+    name: 'Srinu',
+    role: 'Cloud Developer',
+  },
+]
+const App = () => (
+  <div className="list-container">
+    <h1 className="title">Users List</h1>
+    <ul>
+      {userDetailsList.map(eachList => (
+        <UserProfile userDetails={eachList} key={eachList.uniqueNo} />
+      ))}
+    </ul>
+  </div>
+)
+
+export default App
